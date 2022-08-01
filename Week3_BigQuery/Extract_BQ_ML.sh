94dffae999d2a8cb16ce844226adf08395b27471
Martin Palkovic <martin@palkovic.org>
2022-08-01 18:51:40 -0300

    adding lateset version of the shell script

--- Week3_BigQuery/Extract_BQ_ML.sh
+++ Week3_BigQuery/Extract_BQ_ML.sh
@@ -1,4 +1,6 @@
-# steps
+# Extract a machine learning model from Google Cloud storage, 
+# load the data to a docker image and post the results to your
+# localhost Tensorflow API
 # -------------------------------
 
 # authenticate to your Google Cloud login
@@ -20,6 +22,20 @@ gsutil cp -r gs://taxi_ml_testing/tip_model /tmp/model
 mkdir -p serving_dir/tip_model/1
 
 # pull docker Tensorflow image
-docker pull emacski/tensorflow-serving
-docker run -p 8501:8501 --mount type=bind,source=pwd/serving_dir/tip_model,target= /models/tip_model -e MODEL_NAME=tip_model -t emacski/tensorflow-serving &
-curl -d '{"instances": [{"passenger_count":1, "trip_distance":12.2, "PULocationID":"193", "DOLocationID":"264", "payment_type":"2","fare_amount":20.4,"tolls_amount":0.0}]}' -X POST http://localhost:8501/v1/models/tip_model:predict
+docker pull emacski/tensorflow-serving:latest 
+
+# this starts the container but doesnt work
+docker run -t --rm -p 8500:8500 `
+-v `pwd`/serving_dir/tip_model `
+-e MODEL_NAME=tip_model emacski/tensorflow-serving &
+
+# this doesnt work at all
+docker run -p 8500:8500 --network="host" --mount type=bind,source=./Week3_BigQuery/serving_dir/tip_model,target=/models/tip_model -e MODEL_NAME=tip_model -t tensorflow/serving &
+
+# pulled from Slack - doesntt work
+docker run -p 8501:8501 --mount type=bind,source=pwd/serving_dir/tip_model,target=/models/tip_model -e MODEL_NAME=tip_model --platform linux/amd64 -t emacski/tensorflow-serving &
+
+# check status with a GET request
+curl 'http://localhost:8500/v1/models/tip_model'
+
+curl -d '{"instances": [{"passenger_count":1, "trip_distance":12.2, "PULocationID":"193", "DOLocationID":"264", "payment_type":"2","fare_amount":20.4,"tolls_amount":0.0}]}' -X POST http://localhost:8500/v1/models/tip_model:predict
